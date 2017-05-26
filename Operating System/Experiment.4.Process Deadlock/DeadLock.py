bfbc5c5d5cff5780a115c624fc02e6082b081b3c
陈兴雷 <1240858353@qq.com>
2017-05-26 17:21:48 +0800

    Ex4.DeadLock partly finished!

--- Operating System/Experiment.4.Process Deadlock/DeadLock.py
+++ Operating System/Experiment.4.Process Deadlock/DeadLock.py
@@ -8,16 +8,83 @@ import numpy as np
 
 def DeadLock():
     #vectors known
+    process_num = 5
     Resource = [10,5,7]  #all resources sums up
     Max = [[7,5,3],[3,2,2],[9,0,2],[2,2,2],[4,3,3]]
     Allocation = [[0,1,0],[2,0,0],[3,0,2],[2,1,1],[0,0,2]]
     Need = [[7,4,3],[1,2,2],[6,0,0],[0,1,1],[4,3,1]]
-    Avaiable = [] #need to be set
+    #running vector 
+    Available = [] #need to be set
+    temp_available = []
+    temp_available_I = []
+    process_uf = []  #index of process unfinished ,0 -> unfinished
     #answer vector
-    Work = []  #
-    Need = []
-    Allo = []
+    Work = [0,0,0]  
+    #Allo = [0,0,0]
+    ne = []  #need in answer 
     Work_Allo = []
+    Finish = []
+    safe_order = []
+    
+    #for iter in range(len(Available)):
+    #   Work.append(Avaiable[iter])
+    for iter in range(process_num):
+       Finish.append(0)
+    for iter in range(process_num):
+       process_uf.append(0)
+
+    #find the first vector of work
+    temp0 = [0,0,0]
+    for iter1 in range(len(Allocation)):
+      temp1 = Allocation[iter1]
+      for iter2 in range(3): 
+         temp0[iter2] = temp0[iter2] + temp1[iter2]
+    Work = [x - y for x, y in zip(Resource, temp0)] 
+    print 'First work is ',Work[0],'\n'
+    print '---------------------------\n'    
+
+    end_flag = 5 - sum(Finish)
+    while end_flag:
+      print process_uf,'\n'
+      for iter0 in range(process_num):  
+        Allo = [0,0,0]
+        #find the available process under work[iter] and store in temp_available
+        for iter1 in range(process_num):
+            if Need[iter1][0] <= Work[0] and Need[iter1][1] <= Work[1] and Need[iter1][2] <= Work[2] and process_uf[iter1] == 0:
+               temp_available.append(Need[iter1])
+               temp_available_I.append(iter1)
+        #run the first one in work
+        print 'temp index:',temp_available_I,'\n'
+        index = temp_available_I[0]         #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+        safe_order.append(index)
+        print 'safe order is :',safe_order,'\n'
+        print 'len of process:',len(process_uf),' and index:',index,'\n'
+        process_uf[index] = 1 # process index finished 
+        Allo = Allocation[index]
+        ne.append(Need[index])
+
+        #compute the Work_Allo.append(Work[iter0]+Allo[iter0])
+        temp0 = [0,0,0]
+        for iter2 in range(3):
+            temp0[iter2] = Work[iter2] + Allo[iter2]
+        Work_Allo.append(temp0)
+        print 'Work_Allo in order ',iter0,'is:',temp0
+        
+        Work = Work_Allo[iter0]
+        Finish[index] = 1
+        temp_available = [] #clear the temp set 
+        temp_available_I = [] 
+        print 'end flag is ',5 - sum(Finish),'\n'
+        print '---------------------------\n'
+    if len(safe_order) == 5:
+          print 'Safe order generated :',safe_order,'\n'
+    else:
+          print 'Could not generate safe order now!'
+
+DeadLock();    
+
+
+    
     
 
 
--- Operating System/Experiment.4.Process Deadlock/test.py
+++ Operating System/Experiment.4.Process Deadlock/test.py
@@ -1,3 +1,4 @@
+#test file
 #test for 2 dem vector:passed 
 maxnum = 10 
 v = [4,5,6]
@@ -9,10 +10,41 @@ for i in range(3): #add new vector
 for i  in range(3): #output
     for j in range(3):
        print matrix[i][j],'\n'    
-print '--------------------------------'
-#test 2 
-for i  in range(3): #output
-        matrix[i] = matrix[i] - [1,2,3]
-for i  in range(3): #output
-    for j in range(3):
-       print matrix[i][j],'\n' 
+print '-----------------------------------1'
+#test 2   
+#for i  in range(3): #output
+#        matrix[i] = matrix[i] - [1,2,3]   
+#for i  in range(3): #output
+#    for j in range(3):
+#       print matrix[i][j],'\n' 
+
+#print 'vector of 2d length:',len(matrix),'\n'
+
+#print '----------------------------------'
+#test 3:sum up 2 demension vector :passed
+Allocation = [[0,1,0],[2,0,0],[3,0,2],[2,1,1],[0,0,2]]
+temp0 = [0,0,0]
+for iter1 in range(len(Allocation)):
+    temp1 = Allocation[iter1]
+    for iter2 in range(3): 
+       temp0[iter2] = temp0[iter2] + temp1[iter2]
+print temp0,'\n'
+print  '----------------------------------3'
+#test 4
+print [x - y for x, y in zip(temp0, [5,5,5])]  #return the value vector of "a - b"
+print  '----------------------------------4'
+#test 5:
+test5 = [4,3,2,1,0]
+for iter in test5:
+    print Allocation[iter],'\n'
+print  '----------------------------------5'
+#test 6:
+Allocation.pop(2) #remove the third vector in Allocation
+print Allocation,'\n'
+print  '----------------------------------6'
+#test 7:
+Need = [[7,4,3],[1,2,2],[6,0,0],[0,1,1],[4,3,1]]
+for iter in range(5):
+    for iter1 in range(3):
+      print Need[iter][iter1],'\n'
+print  '----------------------------------7'
